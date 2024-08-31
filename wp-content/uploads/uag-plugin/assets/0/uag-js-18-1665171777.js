2a69eb54593070257c482b12dde0b326a00c93a7
RamendraTripathi <109637444+RamendraTripathi@users.noreply.github.com>
2024-08-31 16:33:25 +0530

    Testing Wordpress Build
    Testing Wordpress Build

--- wp-content/uploads/uag-plugin/assets/0/uag-js-18-1665171777.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
-	UAGBTabs.init( '.uagb-block-04966160' );
-	UAGBTabs.anchorTabId( '.uagb-block-04966160' );
-});
-window.addEventListener( 'hashchange', function() {
-	UAGBTabs.anchorTabId( '.uagb-block-04966160' );
-}, false );
- })
